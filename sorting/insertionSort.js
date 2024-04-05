7a1cae822fe5fcc0ff1d56a3cb12108e7d53f4af
John Tuzcu <developer.johntuzcu@gmail.com>
2024-04-04 20:32:50 -0400

    Implement insertionSort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,5 +1,15 @@
-const insertionSort = () => {
-  console.log("Hello world");
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1;
+    while (j >= 0 && arr[j + 1] < arr[j]) {
+      let tmp = arr[j + 1];
+      arr[j + 1] = arr[j];
+      arr[j] = tmp;
+      j -= 1;
+    }
+  }
+
+  return arr;
 };
 
 module.exports = insertionSort;
