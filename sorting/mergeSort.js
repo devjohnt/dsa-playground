9182fcbceec10f05523391e893976fba4a91050a
John Tuzcu <developer.johntuzcu@gmail.com>
2024-04-05 12:09:59 -0400

    Implement merge sort

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -1,3 +1,55 @@
-const mergeSort = (arr) => {};
+function mergeSort(arr, l, r) {
+  if (l < r) {
+    let m = Math.floor((l + r) / 2);
+
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
+  }
+  return arr;
+}
+
+function merge(arr, l, m, r) {
+  let length1 = m - l + 1;
+  let length2 = r - m;
+
+  let L = new Array(length1);
+  let R = new Array(length2);
+
+  for (let i = 0; i < length1; i++) {
+    L[i] = arr[l + i];
+  }
+
+  for (let i = 0; i < length2; i++) {
+    R[i] = arr[m + 1 + i];
+  }
+
+  let i = 0;
+  let j = 0;
+  let k = l;
+
+  while (i < length1 && j < length2) {
+    if (L[i] <= R[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < length1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < length2) {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
+}
 
 module.exports = mergeSort;
--- sorting/mergeSort.test.js
+++ sorting/mergeSort.test.js
@@ -2,15 +2,15 @@ const mergeSort = require("./mergeSort");
 
 describe("mergeSort", () => {
   test("sorts an array of numbers in ascending order", () => {
-    expect(mergeSort([5, 3, 8, 4, 2])).toEqual([2, 3, 4, 5, 8]);
+    expect(mergeSort([5, 3, 8, 4, 2], 0, 4)).toEqual([2, 3, 4, 5, 8]);
   });
 
   test("sorts an already sorted array", () => {
-    expect(mergeSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+    expect(mergeSort([1, 2, 3, 4, 5], 0, 4)).toEqual([1, 2, 3, 4, 5]);
   });
 
   test("sorts an array with negative numbers", () => {
-    expect(mergeSort([-3, -1, -2, -4, -5])).toEqual([-5, -4, -3, -2, -1]);
+    expect(mergeSort([-3, -1, -2, -4, -5], 0, 4)).toEqual([-5, -4, -3, -2, -1]);
   });
 
   test("sorts an empty array", () => {
